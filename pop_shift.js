d87aae69146230d9aab1495e40534512a5a5fe98
Jeongsohee <toy3045@gmail.com>
2022-08-01 23:14:33 +0900

    pop_shift

---
+++ pop_shift.js
@@ -0,0 +1,13 @@
+//배열의 맨 뒤 요소 제거
+const array = ['a','b','c','d'];
+array.pop();
+
+console.log(array);
+//결과 : ["a","b","c"]
+
+//배열의 맨 앞 요소 제거
+const array = ['a','b','c','d'];
+array.shift();
+
+console.log(array);
+//결과:["b","c","d"]
