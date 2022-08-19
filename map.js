22c748ee7d9606e122faa4f5e72014a5b3317cb1
Jeongsohee <toy3045@gmail.com>
2022-08-19 23:45:19 +0900

    map

---
+++ map.js
@@ -0,0 +1,14 @@
+function toWeirdCase(string){
+    var arr = string.split(' ')
+    var fullName = ''
+    arr.forEach(function(v, i){
+        var spl = arr[i].split('');
+        var newSpl = spl.map(function(al, j){
+            if(j % 2 == 0) {
+            // 짝수번째 인덱스 -> 대문자
+            return al.toUpperCase()
+        } else {
+            return al.toLowerCase()
+        }})
+        fullName += ' ' + newSpl.join('')})
+        return fullName.trim()}
